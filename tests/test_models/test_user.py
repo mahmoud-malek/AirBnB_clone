f2aba2120a48d15f74104e5f171b0dba07def90f
mahmoud <information.mahmoud@gmail.com>
2023-12-10 13:40:48 +0200

    yup

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for models/user.py.
-
-Unittest classes:
-    TestUser_instantiation
-    TestUser_save
-    TestUser_to_dict
+""" This module contain testcases for the
+    User class
 """
 import os
 import models
